05b066b2e46b0bd23dc6e9c46df0a7556e2124f2
Gabriel P. <gabrielpereiraemdb@gmail.com>
2026-04-21 13:16:14 -0300

    Lógica do programa feita

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from utils import *
 
-conjunto_1 = set(map(int, input("Digite os números do primeiro conjunto separados por espaço: ").split()))
+while True:
+    print("----- BEM VINDO AO INTERSEÇÃO COM PYTHON! ---\n[1] Iniciar\n[2] Sair")
 
-conjunto_2 = set(map(int, input("Digite os números do segundo conjunto separados por espaço: ").split()))
+    opcao = int(input("Digite um número equivalente a opção desejada:"))
 
-intersecao = []   
-    
-print(f"Conjunto 1:{conjunto_1}")
-print(f"Conjunto 2: {conjunto_2}")
-print(f"A interseção entre os conjuntos 1 e 2 é: {verificar_intersecao(conjunto_1,conjunto_2, intersecao)}")
+    if opcao == 1:
+
+        conjunto_1 = set(map(int, input("Digite os números do primeiro conjunto separados por espaço: ").split()))
+
+        conjunto_2 = set(map(int, input("Digite os números do segundo conjunto separados por espaço: ").split()))
+
+        intersecao = []   
+        
+        print(f"Conjunto 1:{conjunto_1}")
+        print(f"Conjunto 2: {conjunto_2}")
+        print(f"A interseção entre os conjuntos 1 e 2 é: {verificar_intersecao(conjunto_1,conjunto_2, intersecao)}")
+
+    elif opcao == 2:
+        print("Você desejou sair...")
+        break
+
+    else:
+        print("Por favor digite uma opção válida.")
+        continue
